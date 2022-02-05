b3b96e17003c678bf95b00162cf5117514d8def7
matianfu <matianfu@gingerologist.com>
2022-02-06 05:31:30 +0800

    1. first json sent and received. 2. http ota enabled in config. 3. server ready.

--- main/main.c
+++ main/main.c
@@ -3,9 +3,16 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+// #include "lwip/err.h"
+#include "lwip/sockets.h"
+
+#include "cJSON.h"
 
 #include "esp_log.h"
 #include "esp_wifi.h"
+#include "esp_ota_ops.h"
 #include "nvs_flash.h"
 
 #include "esp_netif.h"
@@ -14,53 +21,161 @@
 
 // #include "periph_wifi.h"
 
-static const char *TAG = "roadhill_main";
+#define TCP_PORT (6015)
+
+static const char *TAG = "roadhill:main";
+
+const char hex_char[16] = "0123456789abcdef";
+
+static EventGroupHandle_t events;
 
 static esp_netif_t *sta_netif = NULL;
 static esp_netif_t *ap_netif = NULL;
-static char test_token[32] = "roadhill_test";
-static char prod_token[32] = "roadhill_prod";
-static char *ap_tokens[2] = {test_token, prod_token};
+
+static esp_event_handler_instance_t instance_any_wifi_event;
+static esp_event_handler_instance_t instance_any_ip_event;
+
+static wifi_ap_record_t ap_record[20] = {0};
+
+static char test_token[32] = "juwanke-test";
+static char prod_token[32] = "juwanke";
 static wifi_ap_record_t ap = {0};
-static bool using_testing_ap = false;
+static bool using_test_ap = false;
+static uint8_t sta_mac[6] = {0};
+static uint8_t ap_mac[6] = {0};
+static bool wifi_scanning = false;
 
-esp_err_t wifi_scan(char **tokens, int num_of_tokens, int *token_index,
-                    wifi_ap_record_t *ap) {
-    esp_err_t err;
-    static wifi_ap_record_t ap_record[20] = {};
-    uint16_t max_num_of_ap_records = 20;
-    uint16_t num_of_aps = 0;
+extern const wifi_config_t sta_config_test_default;
+extern const wifi_config_t sta_config_prod_default;
+extern const wifi_config_t ap_config_default;
 
-    err = esp_wifi_scan_get_ap_records(&max_num_of_ap_records, ap_record);
-    if (err != ESP_OK)
-        return err;
+static char tx_buf[4096] = {0};
+static int tx_len = 0;
+static char rx_buf[4096] = {0};
 
-    err = esp_wifi_scan_get_ap_num(&num_of_aps);
-    if (err != ESP_OK)
-        return err;
+#define LINE_LENGTH (256 * 1024)
 
-    *token_index = -1;
-    for (int j = 0; j < num_of_tokens; j++) {
-        for (uint16_t i = 0; i < num_of_aps; i++) {
-            char *ssid_str = (char *)ap_record[i].ssid;
-            if (strstr(ssid_str, tokens[j])) {
-                *token_index = j;
-                if (ap)
-                    memcpy(ap, &ap_record[i], sizeof(wifi_ap_record_t));
-                break;
+static char *line;
+static int llen = 0;
+
+static const char rev_token[] = "**";
+static const char ver_token[] = "00000000";
+static const char sha_token[] = "0000000000000000";
+static const char device_info_tmpl[] =
+    "{\"hardware\":{\"codename\":\"roadhill\",\"revision\":\"**\"},"
+    "\"firmware\":{\"version\":\"00000000\",\"sha256\":\"0000000000000000000000"
+    "000000000000000000000000000000000000000000\"}}\n";
+
+static bool is_semver(const char *str) {
+    if (strlen(str) != 8)
+        return false;
+
+    for (int i = 0; i < 8; i++) {
+        if (str[i] >= '0' && str[i] <= '9')
+            continue;
+        if (str[i] >= 'a' && str[i] <= 'f')
+            continue;
+        if (str[i] >= 'A' && str[i] <= 'F')
+            continue;
+        return false;
+    }
+
+    if (str[6] == 'd' || str[6] == 'e' || str[6] == 'f')
+        return false;
+    if (str[6] == 'D' || str[6] == 'E' || str[6] == 'F')
+        return false;
+    return true;
+}
+
+static void prepare_device_info() {
+    char *str;
+    strcpy(tx_buf, device_info_tmpl);
+
+    str = strstr(tx_buf, rev_token);
+    str[0] = 'a';
+    str[1] = '0';
+
+    const esp_app_desc_t *app_desc = esp_ota_get_app_description();
+    if (is_semver(app_desc->version)) {
+        str = strstr(tx_buf, ver_token);
+        for (int i = 0; i < 8; i++) {
+            *str++ = app_desc->version[i];
+        }
+    }
+
+    uint8_t sha[32];
+    const esp_partition_t *part = esp_ota_get_running_partition();
+    if (part) {
+        esp_err_t err = esp_partition_get_sha256(part, sha);
+        if (err == ESP_OK) {
+            str = strstr(tx_buf, sha_token);
+            for (int i = 0; i < 32; i++) {
+                str[2 * i] = hex_char[sha[i] / 16];
+                str[2 * i + 1] = hex_char[sha[i] % 16];
             }
         }
+    }
+    tx_len = strlen(tx_buf);
+}
 
-        if (*token_index >= 0) {
+static int process_line() {
+    ESP_LOGI(TAG, "line: %s", line);
+    cJSON *root = cJSON_Parse(line);
+    // should we raise error if not parsed correctly? TODO
+    if (root) {
+        char *cmd = cJSON_GetObjectItem(root, "cmd")->valuestring;
+        if (0 == strcmp(cmd, "ota")) {
+            char *url = cJSON_GetObjectItem(root, "url")->valuestring; 
+            ESP_LOGI(TAG, "ota url: %s", url);
+        }
+        cJSON_Delete(root);
+    }
+    return 0;
+}
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data) {
+    if (wifi_scanning)
+        return;
+
+    if (event_base == WIFI_EVENT) {
+        switch (event_id) {
+        case WIFI_EVENT_STA_START:
+            ESP_LOGI(TAG, "wifi event: sta_start, connecting to ap");
+            esp_wifi_connect();
+            break;
+        case WIFI_EVENT_STA_DISCONNECTED:
+            ESP_LOGI(TAG, "wifi event: sta_disconnected, reconnecting to ap");
+            esp_wifi_connect();
+        default:
             break;
         }
     }
 
-    return ESP_OK;
+    if (event_base == IP_EVENT) {
+        switch (event_id) {
+        case IP_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(events, (EventBits_t)1);
+            break;
+        case IP_EVENT_STA_LOST_IP:
+            xEventGroupClearBits(events, (EventBits_t)1);
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 void app_main(void) {
+    // esp_err_t is typedef-ed int, so it could be used with lwip/sockets
+    // api, but the value should be interpretted differently.
     esp_err_t err;
+    int i, j;
+
+    events = xEventGroupCreate();
+
+    line = (char *)malloc(LINE_LENGTH);
+    memset(line, 0, LINE_LENGTH);
 
     // init nvs
     err = nvs_flash_init();
@@ -71,44 +186,204 @@ void app_main(void) {
         err = nvs_flash_init();
     }
 
-    // init event loop and wifi
+    // init event loop
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // this operations can not be done
+    // otherwise it competes with scanning
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL,
+        &instance_any_wifi_event));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        IP_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL,
+        &instance_any_ip_event));
+
+    // init sta interface and mac
     ESP_ERROR_CHECK(esp_netif_init());
     sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
+
+    // init ap interface and mac
     ap_netif = esp_netif_create_default_wifi_ap();
     assert(ap_netif);
 
-    ESP_LOGI(TAG, "size of index: %d", sizeof(index));
-
+    // init wifi
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+
+    // get mac and log
+    ESP_ERROR_CHECK(esp_wifi_get_mac(WIFI_IF_STA, sta_mac));
+    ESP_LOGI(TAG, "sta mac: %02x:%02x:%02x:%02x:%02x:%02x", sta_mac[0],
+             sta_mac[1], sta_mac[2], sta_mac[3], sta_mac[4], sta_mac[5]);
+    ESP_ERROR_CHECK(esp_wifi_get_mac(WIFI_IF_AP, ap_mac));
+    ESP_LOGI(TAG, "ap mac: %02x:%02x:%02x:%02x:%02x:%02x", ap_mac[0], ap_mac[1],
+             ap_mac[2], ap_mac[3], ap_mac[4], ap_mac[5]);
+
+    // start wifi for scanning
+    wifi_scanning = true;
     ESP_ERROR_CHECK(esp_wifi_start());
 
     // loop forever if neither test or prod ap found
     while (1) {
-        int token_index;
+        uint16_t max_num_of_ap_records = 20;
+        uint16_t num_of_aps = 0;
+
         memset(&ap, 0, sizeof(ap));
-        ESP_ERROR_CHECK(wifi_scan(ap_tokens, 2, &token_index, &ap));
-        if (token_index >= 0) {
-            using_testing_ap = !!(token_index == 0);
-            if (using_testing_ap) {
-                ESP_LOGI(TAG, "found test ap: %s", (char *)ap.ssid);
-            } else {
-                ESP_LOGI(TAG, "found prod ap: %s", (char *)ap.ssid);
-            }
-            break;
-        } else {
-            ESP_LOGI(TAG, "neither test nor prod ap found.");
-            vTaskDelay(3000 / portTICK_PERIOD_MS);
+        ESP_ERROR_CHECK(esp_wifi_scan_start(NULL, true));
+        ESP_ERROR_CHECK(
+            esp_wifi_scan_get_ap_records(&max_num_of_ap_records, ap_record));
+        ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&num_of_aps));
+
+        for (i = 0; i < num_of_aps; i++) {
+            ESP_LOGI(TAG, "ap[%d].ssid: %s", i, (char *)ap_record[i].ssid);
         }
+
+        // find test ap token
+        for (i = 0; i < num_of_aps; i++) {
+            char *ssid_str = (char *)ap_record[i].ssid;
+            if (strstr(ssid_str, test_token)) {
+                memcpy(&ap, &ap_record[i], sizeof(wifi_ap_record_t));
+                using_test_ap = true;
+                break;
+            }
+        }
+
+        if (i < num_of_aps)
+            break;
+
+        // find prod ap (exact match)
+        for (i = 0; i < num_of_aps; i++) {
+            char *ssid_str = (char *)ap_record[i].ssid;
+            if (0 == strcmp(ssid_str, prod_token)) {
+                memcpy(&ap, &ap_record[i], sizeof(wifi_ap_record_t));
+                break;
+            }
+        }
+
+        if (i < num_of_aps)
+            break;
+
+        ESP_LOGI(TAG, "neither test nor prod ap found.");
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
     }
 
-    // now we have the ssid
+    ESP_ERROR_CHECK(esp_wifi_stop());
+    wifi_scanning = false;
 
+    if (using_test_ap) {
+        ESP_LOGI(TAG, "found test ap: %s", (char *)ap.ssid);
+    } else {
+        ESP_LOGI(TAG, "found prod ap: %s", (char *)ap.ssid);
+    }
+
+    // now we have the ssid, try to establish tcp connection with server
+    // in case of prod ap, use ????
+    // in case of test ap, use port 6015
+    wifi_config_t sta_cfg;
+    if (using_test_ap) {
+        sta_cfg = sta_config_test_default;
+    } else {
+        sta_cfg = sta_config_prod_default;
+    }
+    strlcpy((char *)sta_cfg.sta.ssid, (char *)ap.ssid,
+            sizeof(sta_cfg.sta.ssid));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_cfg));
+
+    wifi_config_t ap_cfg = ap_config_default;
+    strlcpy((char *)ap_cfg.ap.ssid, "juwanke-soundbar-",
+            sizeof(ap_cfg.ap.ssid));
+
+    j = strlen("juwanke-soundbar-");
+    for (i = 0; i < 6; i++) {
+        ap_cfg.ap.ssid[j++] = hex_char[sta_mac[i] / 16];
+        ap_cfg.ap.ssid[j++] = hex_char[sta_mac[i] % 16];
+    }
+
+    ESP_LOGI(TAG, "ap ssid: %s", (char *)ap_cfg.ap.ssid);
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap_cfg));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    // (re-)connection in loop
     while (1) {
-        ESP_LOGI(TAG, "do nothing");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        xEventGroupWaitBits(events, (EventBits_t)1, pdFALSE, pdFALSE,
+                            portMAX_DELAY);
+
+        esp_netif_ip_info_t ip_info;
+        err = esp_netif_get_ip_info(sta_netif, &ip_info);
+        if (err != ESP_OK) {
+            ESP_LOGI(TAG, "failed to get ip info");
+            goto closed;
+        }
+
+        int tcp_port = 8080;
+
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_addr.s_addr = ip_info.gw.addr;
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(tcp_port);
+        int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+        if (sock < 0) {
+            ESP_LOGI(TAG, "failed to create socket");
+            goto closed;
+        }
+
+        err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        if (err < 0) {
+            ESP_LOGI(TAG, "failed to connect to tcp server (%d)", errno);
+            goto closing;
+        } else {
+            ESP_LOGI(TAG, "connected to tcp server");
+        }
+
+        prepare_device_info();
+
+        if (tx_len) {
+            int start = 0;
+            while (1) {
+                int sent = send(sock, &tx_buf[start], tx_len - start, 0);
+                if (sent < 0) {
+                    ESP_LOGI(TAG, "send error (%d)", errno);
+                    goto closing;
+                }
+                start += sent;
+                if (start < tx_len) {
+                    vTaskDelay(0);
+                } else {
+                    break;
+                }
+            };
+        }
+
+        while (1) {
+            int len = recv(sock, rx_buf, sizeof(rx_buf), 0);
+            if (len < 0) {
+                ESP_LOGI(TAG, "recv error (%d)", errno);
+                goto closing;
+            }
+
+            for (int i = 0; i < len; i++) {
+                if (rx_buf[i] == '\r' || rx_buf[i] == '\n') {
+                    if (llen > 0) {
+                        line[llen] = '\0';
+                        if (process_line()) {
+                            goto closing;
+                        }
+                        llen = 0;
+                    }
+                } else {
+                    line[llen++] = rx_buf[i];
+                    if (llen >= LINE_LENGTH - 1) {
+                        ESP_LOGI(TAG, "received line too long");
+                        goto closing;
+                    }
+                }
+            }
+        }
+
+    closing:
+        close(sock);
+    closed:
+        vTaskDelay(8000 / portTICK_PERIOD_MS);
     }
 }
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const net = require('net')
 const express = require('express')
 const app = express()
 
+const tcp = net.createServer(socket => {
+  console.log(`client connected, local: ${socket.localAddress}, remote: ${socket.remoteAddress}`)
+
+  socket.on('data', data => {
+    console.log(data.toString())
+    const msg = JSON.stringify({cmd: "ota",url: "http://10.42.0.1/files/roadhill.bin"}) + '\n'
+    console.log(msg)
+    socket.write(msg)
+  })
+})
+
+tcp.listen(8080, '10.42.0.1', () => {
+  console.log('tcp server started, listening 10.42.0.1 @ 8080')
+})
+
 app.use('/files', express.static('files'))
 
-app.listen(80, "10.42.0.1", () => {
-    console.log("hello")    
+app.listen(80, '10.42.0.1', () => {
+  console.log('http server started, listening 10.42.0.1 @80, files located in /files')
 })
