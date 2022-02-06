2ea75e99620b4e5655c294df4b748788db5c14a2
matianfu <matianfu@gingerologist.com>
2022-02-06 21:13:26 +0800

    update server.js for roadhill.bin ota upgrade

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
+const fs = require('fs')
 const net = require('net')
 const express = require('express')
 const app = express()
 
-const tcp = net.createServer(socket => {
-  console.log(`client connected, local: ${socket.localAddress}, remote: ${socket.remoteAddress}`)
+fs.cp('build/roadhill.bin', 'files/roadhill.bin', err => {
+  if (err) return
 
-  socket.on('data', data => {
-    console.log(data.toString())
-    const msg = JSON.stringify({cmd: "ota",url: "http://10.42.0.1/files/roadhill.bin"}) + '\n'
-    console.log(msg)
-    socket.write(msg)
+  fs.readFile('files/roadhill.bin', (err, content) => {
+    if (err) return
+
+    const sha256 = content.slice(-32).toString('hex')
+    console.log(`roadhill.bin sha256: ${sha256}`)
+
+    const tcp = net.createServer(socket => {
+      console.log(`client connected, local: ${socket.localAddress}, remote: ${socket.remoteAddress}`)
+
+      socket.on('data', data => {
+        const text = data.toString()
+        try {
+          const msg = JSON.parse(text)
+
+          console.log('incoming <-', msg)
+
+          if (msg.type === 'DEVICE_INFO') {
+            if (msg.firmware.sha256 !== sha256) {
+              const outgoing = JSON.stringify({ cmd: 'ota', url: 'http://10.42.0.1/files/roadhill.bin' }) + '\n'
+              console.log(`outgoing -> ${outgoing}`)
+              socket.write(outgoing)
+            }
+          }
+        } catch (e) {
+
+        }
+      })
+    })
+
+    tcp.listen(8080, '10.42.0.1', () => {
+      console.log('tcp server started, listening 10.42.0.1 @ 8080')
+    })
+
+    app.use('/files', express.static('files'))
+    app.listen(80, '10.42.0.1', () => {
+      console.log('http server started, listening 10.42.0.1 @ 80, files located in /files')
+    })
   })
-})
-
-tcp.listen(8080, '10.42.0.1', () => {
-  console.log('tcp server started, listening 10.42.0.1 @ 8080')
-})
-
-app.use('/files', express.static('files'))
-
-app.listen(80, '10.42.0.1', () => {
-  console.log('http server started, listening 10.42.0.1 @80, files located in /files')
 })
