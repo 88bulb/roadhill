a75799a8229d1a4ed458ce73ce4a787455909bf7
matianfu <matianfu@gingerologist.com>
2022-02-14 03:03:17 +0800

    add an blink example to server.js

--- server.js
+++ server.js
@@ -36,16 +36,20 @@ fs.cp('build/roadhill.bin', 'files/roadhill.bin', err => {
               const outgoing = JSON.stringify({
                 cmd: 'PLAY',
                 version: '1.0',
-// http://shinuojbs-1306173101.cos.ap-shanghai.myqcloud.com/sgd_audio/8b76c5faf3fd66b66d5a36e6ffe9e7bb.mp3
-
-//                tracks_url: 'http://10.42.0.1/files/album000001',
+                // tracks_url: 'http://shinuojbs-1306173101.cos.ap-shanghai.myqcloud.com/sgd_audio/8b76c5faf3fd66b66d5a36e6ffe9e7bb.mp3',
+                // tracks_url: 'http://10.42.0.1/files/album000001',
                 tracks_url: 'http://shinuojbs-1306173101.cos.ap-shanghai.myqcloud.com/sgd_audio',
                 tracks: [{
                   name: '8b76c5faf3fd66b66d5a36e6ffe9e7bb.mp3',
                   size: 6817965
                 }],
-                lighting_time_unit: 'sec',
-                lighting: [],
+                blinks: [
+                  {
+                    time: 0,
+                    mask: 'ffff',
+                    code: '100300000003c680c6f0fa33f0fafa'
+                  }
+                ],
                 start: 'immediate'
               }) + '\n'
               console.log(`outgoing -> ${outgoing}`)
